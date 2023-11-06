4a968e6d7ac2127ff8e7729aa8b474faa53ab549
Kevin Egstorf <Kevin.Egstorf@gmail.com>
2023-11-06 19:46:41 +0100

    feat: updated start script

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ const app: Application = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (_req: Request, res: Response) => {
+  res.statusCode = 200;
   res.send('Welcome to Express & TypeScript Server');
 });
 
