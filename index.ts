d9a9485c4cd4782b6e3ea16857b6134f64136aed
Kevin Egstorf <Kevin.Egstorf@gmail.com>
2023-11-06 19:19:44 +0100

    feat: first commit 🎉

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, Application } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fired at http://localhost:${port}`);
+});
